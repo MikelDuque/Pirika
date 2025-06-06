7d6edb5ca61e46665e9ef3287da333b31d02fbf4
Mikel <mikel.0205.navarra@gmail.com>
2025-06-06 10:13:13 +0200

    Maaaas pruebas

--- Server/scripts/beforeInstall.sh
+++ Server/scripts/beforeInstall.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -e
-
-exec > >(tee -a /var/log/codedeploy/before_install.log) 2>&1
+pwd > /var/log/hola.txt
 
 #ACTUALIZACION PROYECTO
 rm -rf /opt/pirika/publish
 mkdir -p /opt/pirika/publish
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Server
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/*/Server
 dotnet publish Server.csproj -c Release -o ./publish
