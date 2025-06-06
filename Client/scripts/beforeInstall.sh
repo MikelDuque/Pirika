e9d998ba56a9923c1a048c9f6d9971746b6d0259
Mikel <mikel.0205.navarra@gmail.com>
2025-06-06 11:02:16 +0200

    mierda

--- Client/scripts/beforeInstall.sh
+++ Client/scripts/beforeInstall.sh
@@ -2,8 +2,8 @@
 set -e
 
 #ACTUALIZACION PROYECTO
-rm -rf /opt/pirika
-mkdir -p /opt/pirika
+rm -rf /var/www/pirika
+mkdir -p /var/www/pirika
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Client
 npm install
 npm run build
