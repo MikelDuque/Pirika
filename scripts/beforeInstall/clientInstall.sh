9bf34b74df0818b64e72d69965fe765cbf650127
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-06-10 20:59:21 +0200

    A ver si ya...

--- scripts/beforeInstall/clientInstall.sh
+++ scripts/beforeInstall/clientInstall.sh
@@ -5,7 +5,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Client_Deploy" ]; then
   #ACTUALIZACION PROYECTO
   rm -rf /var/www/pirika
   mkdir -p /var/www/pirika
-  cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/*/Client
+  cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Client
   npm install
   npm run build
 
--- scripts/beforeInstall/serverInstall.sh
+++ scripts/beforeInstall/serverInstall.sh
@@ -5,7 +5,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Server_Deploy" ]; then
   #ACTUALIZACION PROYECTO
   rm -rf /opt/pirika/publish
   mkdir -p /opt/pirika/publish
-  cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/*/Server
+  cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Server
   dotnet publish Server.csproj -c Release -o ./publish
   
 fi
