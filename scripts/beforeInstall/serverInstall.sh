6c707540db839f59c3e08a84e572b9a133671e21
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-06-10 21:55:34 +0200

    ultimos arreglillos (espero)

--- scripts/beforeInstall/serverInstall.sh
+++ scripts/beforeInstall/serverInstall.sh
@@ -3,9 +3,11 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Server_Deploy" ]; then
   set -e
 
   #ACTUALIZACION PROYECTO
-  rm -rf /var/www/pirika
-  mkdir -p /var/www/pirika
+  rm -rf /opt/pirika/publish
+  mkdir -p /opt/pirika/publish
   cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Server
-  sudo dotnet publish Server.csproj -c Release -o ../Build
+  sudo dotnet publish Server.csproj -c Release -o /opt/pirika/publish
   
+  #EVITAR FALLOS
+  mkdir ../Client/dist
 fi
