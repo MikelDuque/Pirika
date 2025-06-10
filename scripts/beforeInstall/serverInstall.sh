55b0453a3be4dfc688337a37e2f1da7065e01951
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-06-10 21:35:54 +0200

    Tratando de solucionar el problema del install

--- scripts/beforeInstall/serverInstall.sh
+++ scripts/beforeInstall/serverInstall.sh
@@ -2,14 +2,10 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Server_Deploy" ]; then
   set -e
 
-  echo $HOME > /tmp/hola.txt
-  echo $DOTNET_ROOT >> /tmp/hola.txt
-  echo $PATH >> /tmp/hola.txt
-
   #ACTUALIZACION PROYECTO
   rm -rf /opt/pirika/publish
   mkdir -p /opt/pirika/publish
   cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Server
-  sudo dotnet publish Server.csproj -c Release -o ./publish
+  sudo dotnet publish Server.csproj -c Release -o ../Build
   
 fi
