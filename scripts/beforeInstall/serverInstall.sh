63f255b83571fc52fda5ae2b70493c10f2bcac8e
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-06-10 21:42:47 +0200

    más intentos

--- scripts/beforeInstall/serverInstall.sh
+++ scripts/beforeInstall/serverInstall.sh
@@ -3,8 +3,8 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Server_Deploy" ]; then
   set -e
 
   #ACTUALIZACION PROYECTO
-  rm -rf /opt/pirika/publish
-  mkdir -p /opt/pirika/publish
+  rm -rf /var/www/pirika
+  mkdir -p /var/www/pirika
   cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/Server
   sudo dotnet publish Server.csproj -c Release -o ../Build
   
