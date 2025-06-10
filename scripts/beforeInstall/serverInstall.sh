1c98aaee35efa870697fd176f0e05d239cb92856
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-06-10 20:56:10 +0200

    FUNcIONA, + pruebas

--- scripts/beforeInstall/serverInstall.sh
+++ scripts/beforeInstall/serverInstall.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 if [ "$DEPLOYMENT_GROUP_NAME" == "Pirika_Server_Deploy" ]; then
   set -e
-  pwd > /tmp/hola.log
 
   #ACTUALIZACION PROYECTO
-  # rm -rf /opt/pirika/publish
-  # mkdir -p /opt/pirika/publish
-  # cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/*/Server
-  # dotnet publish Server.csproj -c Release -o ./publish
+  rm -rf /opt/pirika/publish
+  mkdir -p /opt/pirika/publish
+  cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/*/Server
+  dotnet publish Server.csproj -c Release -o ./publish
   
 fi
